e803e95b2d50b8cfde052766bd2c0a295e9cbbd3
Daria_Serebriakova <dserebri@estee.com>
2024-08-07 14:27:08 +0200

    FunctionalInterface

---
+++ src/main/java/org/daria/serebriakova/util/MathOperation.java
@@ -0,0 +1,26 @@
+package org.daria.serebriakova.util;
+
+@FunctionalInterface
+public interface MathOperation {
+    double operate(double a, double b);
+
+    // Default method
+    default double add(double a, double b) {
+        return a + b;
+    }
+
+    // Static method
+    static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // Private method (Java 9 and later)
+    private double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Default method using the private method
+    default double multiplyAndAdd(double a, double b, double c) {
+        return add(multiply(a, b), c);
+    }
+}
---
+++ src/test/java/org/daria/serebriakova/util/MathOperationTest.java
@@ -0,0 +1,37 @@
+package org.daria.serebriakova.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class MathOperationTest {
+
+    @Test
+    public void canImplementAbstractMethod() {
+        MathOperation division = (a, b) -> a / b;
+
+        double actual = division.operate(10, 2);
+
+        assertThat(actual).isEqualTo(5);
+    }
+
+    @Test
+    public void canCallDefaultMethod() {
+        MathOperation multiply = (a, b) -> a * b;
+
+        double actual =  multiply.add(multiply.operate(5, 2), 5);
+        double actualUsingMultiplyAndAdd =  multiply.multiplyAndAdd(5,2,5);
+
+        assertThat(actual).isEqualTo(actualUsingMultiplyAndAdd);
+    }
+
+    @Test
+    public void canCallStaticMethod() {
+        double actual = MathOperation.subtract(10, 2);
+
+        assertThat(actual).isEqualTo(8);
+    }
+}
